fe2275bd9f6576a34888058642ee17c6d17d47b1
morita657 <morita657@gmail.com>
2020-09-01 14:48:41 +0900

    Fix method annotation on mapper

--- src/main/java/com/example/cloudstorage/mapper/NoteMapper.java
+++ src/main/java/com/example/cloudstorage/mapper/NoteMapper.java
@@ -1,10 +1,7 @@
 package com.example.cloudstorage.mapper;
 
 import com.example.cloudstorage.model.Note;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Options;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,12 +12,18 @@ public interface NoteMapper {
     @Select("select * from notes")
     List<Note> getAllNotes();
 
-    @Select("insert into Notes (notetitle, notedescription, userid) values (#{note.noteTitle}, #{note.noteDescription}, #{userid})")
+    @Insert("insert into Notes (notetitle, notedescription, userid) values (#{note.noteTitle}, #{note.noteDescription}, #{userid})")
     @Options(useGeneratedKeys = true, keyProperty = "noteId")
     Integer create(@Param("note") Note note , Long userid);
 
 
-    @Select("update Notes set notetitle=#{note.noteTitle}, notedescription= #{note.noteDescription} where userid = #{userid} and noteid = #{noteid}")
+    @Update("update Notes set notetitle=#{note.noteTitle}, notedescription= #{note.noteDescription} where userid = #{userid} and noteid = #{noteid}")
     @Options(useGeneratedKeys = true, keyProperty = "noteId")
     Integer update(@Param("note") Note note , Long userid, Long noteid);
+
+
+
+    @Delete("delte from Notes where userid = #{userid} and noteid = #{noteid}")
+    @Options(useGeneratedKeys = true, keyProperty = "noteId")
+    Integer delete(@Param("note") Long userid, Long noteid);
 }
