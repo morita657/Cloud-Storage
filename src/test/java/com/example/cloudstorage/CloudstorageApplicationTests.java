4216688ac042bb96a197afd0f915c9284a47f8bb
morita657 <morita657@gmail.com>
2020-09-05 20:10:52 +0900

    Build dashboard access test w/o access

--- src/test/java/com/example/cloudstorage/CloudstorageApplicationTests.java
+++ src/test/java/com/example/cloudstorage/CloudstorageApplicationTests.java
@@ -1,13 +1,36 @@
 package com.example.cloudstorage;
 
-import org.junit.jupiter.api.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CloudstorageApplicationTests {
+	@LocalServerPort
+	private Integer port;
+
+	private static WebDriver driver;
+//	private CounterPage counter;
+
+	@BeforeAll
+	public static void beforeAll(){
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+	}
+
+	@AfterAll
+	public static void afterAll(){
+		driver.quit();
+	}
+
 
 	@Test
-	void contextLoads() {
+	@Order(1)
+	public void dashboardAccessWithoutLogin(){
+		driver.get(String.format("http://localhost:%s/dashboard",port));
 	}
 
 }
