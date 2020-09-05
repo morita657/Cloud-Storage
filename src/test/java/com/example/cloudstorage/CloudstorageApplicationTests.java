d3967a814792c2824a2dc531f3876b1140b1e21a
morita657 <morita657@gmail.com>
2020-09-05 21:30:15 +0900

    Add trySignupLoginLogoutAndAccessHomeAgain test

--- src/test/java/com/example/cloudstorage/CloudstorageApplicationTests.java
+++ src/test/java/com/example/cloudstorage/CloudstorageApplicationTests.java
@@ -63,16 +63,55 @@ class CloudstorageApplicationTests {
 		assertThat(successMsg.getText().contains("You successfully signed up!"));
 	}
 
+	private void _loginInput(String username, String password){
+		assertThat(driver.getTitle()).isEqualTo("Login");
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+
+		WebElement usernameInputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputUsername")));
+		usernameInputField.sendKeys(username);
+
+		WebElement passwordInputField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputPassword")));
+		passwordInputField.sendKeys(password);
+
+		WebElement signUpButton = driver.findElement(By.id("buttonLogin"));
+		wait.until(ExpectedConditions.elementToBeClickable(signUpButton)).submit();
+	}
+
+	private void _logoutClick(){
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+
+		WebElement logoutButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("buttonLogout")));
+		assertThat(driver.getTitle()).isEqualTo("Home");
+		logoutButton.submit();
+		System.out.println("logout lickc..");
+	}
+
+	private void _accessHome(){
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputUsername")));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("inputPassword")));
+		driver.get(String.format("http://localhost:%s/dashboard", this.port));
+		assertThat(driver.getTitle()).isEqualTo("Login");
+	}
+
 	@Test
 	@Order(2)
-	public void signup(){
+	public void trySignupLoginLogoutAndAccessHomeAgain(){
 		driver.get(String.format("http://localhost:%s/signup",port));
 		assertThat(driver.getTitle()).isEqualTo("Sign Up");
 		String firstName = "fsfsdf";
 		String lastName = "dfsd";
 		String username = "fsdf";
 		String password = "fsufs";
+		//		signup
 		_signupInput(firstName, lastName, username, password);
+		//		login
+		driver.get(String.format("http://localhost:%s/login", this.port));
+		_loginInput(username, password);
+		//		logout
+		_logoutClick();
+		//		try to access home page
+		_accessHome();
 	}
 
 }
