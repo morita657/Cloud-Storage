c004d3f50077082f08741d2b02e3a9ee2bf67e14
morita657 <morita657@gmail.com>
2020-09-18 19:00:12 +0900

    Add success message

--- src/main/java/com/example/cloudstorage/controller/AuthController.java
+++ src/main/java/com/example/cloudstorage/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class AuthController {
@@ -22,7 +23,7 @@ public class AuthController {
     public String signUp(){return "signup";}
 
     @PostMapping(value = "/register")
-    public String register(@ModelAttribute("SpringWeb") User user){
+    public String register(@ModelAttribute("SpringWeb") User user, RedirectAttributes redirectAttributes){
         if (user == null){
             return "redirect:signup";
         }
@@ -34,6 +35,8 @@ public class AuthController {
             return "redirect:signup?error";
         }
         System.out.println("signup done");
+        redirectAttributes.addFlashAttribute("message", "Success");
+
         return "redirect:login?success";
     }
 }
